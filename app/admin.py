2369d79b2acd8eae81564f760e12f6ecf5e7bd07
Roni Rissanen <roni.rissanen@student.careeria.fi>
2022-06-29 12:08:05 +0300

    admin model register

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Supplier, Product, Category, Employee, Customer, Order
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin): # Sekä rekisteröidä kyseisellä metodilla
+    pass
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin): # Sekä rekisteröidä kyseisellä metodilla
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin): # Sekä rekisteröidä kyseisellä metodilla
+    pass
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin): # Sekä rekisteröidä kyseisellä metodilla
+    pass
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin): # Sekä rekisteröidä kyseisellä metodilla
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin): # Sekä rekisteröidä kyseisellä metodilla
+    pass
